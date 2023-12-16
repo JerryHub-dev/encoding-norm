4aff8505f6593f5522293ddd647433839cc7a857
lishuai <Jerry4415@foxmail.com>
2023-12-17 01:19:37 +0800

    feat: dumirc

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'encoding-norm'; // 项目名
+
 export default defineConfig({
+  title: 'encoding-norm',
+  mode: 'site',
+  devServer: {
+    port: 1998 // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  
+  // more config: https://d.umijs.org/config
   themeConfig: {
     name: '编码规范',
   },
